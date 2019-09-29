f6067c9f751ae55d4fa4cbe62e52c1db2c227b94
Max <max30012540@hotmail.com>
2019-09-29 18:16:17 +0700

    แก้เป็นแบบฟังก์ชั่น

--- simulator/compiler.js
+++ simulator/compiler.js
@@ -24,98 +24,101 @@ let includeDir = [
   "D:\\GitHub\\IOXGD-Designer\\simulator\\pc_simulator_win_codeblocks\\lvgl"
 ];
 
-let include = {
-  c: includeDir.map(x => `-I${x}`).join(' ')
+let clean = () => {
+  let deleteFolderRecursive = (path) => {
+    if (fs.existsSync(path)) {
+      fs.readdirSync(path).forEach(function(file, index){
+        var curPath = path + "/" + file;
+        if (fs.lstatSync(curPath).isDirectory()) { // recurse
+          deleteFolderRecursive(curPath);
+        } else { // delete file
+          fs.unlinkSync(curPath);
+        }
+      });
+      fs.rmdirSync(path);
+    }
+  };
+
+  deleteFolderRecursive(outputDir);
 }
 
-var deleteFolderRecursive = function(path) {
-  if (fs.existsSync(path)) {
-    fs.readdirSync(path).forEach(function(file, index){
-      var curPath = path + "/" + file;
-      if (fs.lstatSync(curPath).isDirectory()) { // recurse
-        deleteFolderRecursive(curPath);
-      } else { // delete file
-        fs.unlinkSync(curPath);
+let compile = (finishCallback) => {
+  var walkSync = function(dir, filelist) {
+    files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+      if (fs.statSync(dir + '/' + file).isDirectory()) {
+        filelist = walkSync(dir + '/' + file, filelist);
+      }
+      else {
+        filelist.push(dir + '/' + file);
       }
     });
-    fs.rmdirSync(path);
+    return filelist;
+  };
+
+  let listCodeFile = walkSync(projectDir).filter(f => f.endsWith(".cpp") || f.endsWith(".c") || f.endsWith(".s") || f.endsWith(".S"));
+
+  outputFiles = [];
+  completeCounter = 0;
+  listCodeFile.forEach((file) => {
+    let outDir = path.dirname(file).replace(projectDir, outputDir);
+    let fileName = path.basename(file);
+
+    // Check exist output dir
+    if (!fs.existsSync(outDir)) {
+      fs.mkdirSync(outDir, { recursive: true });
+    }
+
+    if (file.endsWith(".c")) {
+      let outFile = `${outDir}/${fileName}.o`;
+
+      let cmd = `${compilerDir}/${compiler.c} ${flag.c} ${includeDir.map(x => `-I "${x}"`).join(' ')} -c "${file}" -o "${outFile}"`;
+      exec(cmd, {cwd: compilerDir }, function(error, stdout, stderr) {
+        if (error) {
+          console.log(`Compile ${fileName} fail`);
+          console.error(stderr);
+          console.log(stdout);
+          process.exit(0);
+          return;
+        }
+
+        console.log(`Compile ${fileName} OK`);
+        completeCounter++;
+
+        if (completeCounter == listCodeFile.length) {
+          getExe();
+        }
+      });
+      outputFiles.push(outFile);
+    }
+  });
+
+  let getExe = function() {
+    let arg = [];
+    arg = arg.concat(flag.linker.split(' '));
+    arg = arg.concat(outputFiles);
+    arg.push('-o');
+    arg.push(`${outputDir}/${outputFile}`);
+
+    execFile(`${compilerDir}/${compiler.linker}`, arg, {cwd: compilerDir }, function(error, stdout, stderr) {
+      if (error) {
+        console.log("Get Exe error !");
+        console.error(stderr);
+        console.log(stdout);
+      } else {
+        // console.log("Finish !");
+        if (typeof finishCallback === "function") {
+          finishCallback();
+        }
+      }
+    });
   }
-};
+}
 
 console.log("Clean");
-deleteFolderRecursive(outputDir);
+clean();
 
-var walkSync = function(dir, filelist) {
-  files = fs.readdirSync(dir);
-  filelist = filelist || [];
-  files.forEach(function(file) {
-    if (fs.statSync(dir + '/' + file).isDirectory()) {
-      filelist = walkSync(dir + '/' + file, filelist);
-    }
-    else {
-      filelist.push(dir + '/' + file);
-    }
-  });
-  return filelist;
-};
+console.log("Compile...");
+compile(() => console.log("Finish !"));
 
-let listCodeFile = walkSync(projectDir).filter(f => f.endsWith(".cpp") || f.endsWith(".c") || f.endsWith(".s") || f.endsWith(".S"));
-
-let outputFiles = [];
-let cmd = [];
-listCodeFile.forEach((file) => {
-  let outDir = path.dirname(file).replace(projectDir, outputDir);
-  let fileName = path.basename(file);
-
-  // Check exist output dir
-  if (!fs.existsSync(outDir)) {
-    fs.mkdirSync(outDir, { recursive: true });
-  }
-
-  if (file.endsWith(".c")) {
-    let outFile = `${outDir}/${fileName}.o`;
-
-    cmd.push(`${compilerDir}/${compiler.c} ${flag.c} ${include.c} -c ${file} -o ${outFile}`);
-    outputFiles.push(outFile);
-  }
-});
-
-// cmd.push(`${compilerDir}/${compiler.linker} ${flag.linker} ${outputFiles.join(' ')} -o ${outputDir}/${outputFile}`);
-
-let getExe = function() {
-  arg = `${flag.linker} ${outputFiles.join(' ')} -o ${outputDir}/${outputFile}`;
-  execFile(`${compilerDir}/${compiler.linker}`, arg.split(' '), {cwd: compilerDir }, function(error, stdout, stderr) {
-    if (error) {
-      console.log("Get Exe error !");
-      console.error(stderr);
-      console.log(stdout);
-    } else {
-      console.log("Finish !");
-
-      // exec(`${outputDir}/${outputFile}`, {cwd: compilerDir });
-    }
-  });
-}
-
-let complie = function(list, index) {
-  index = index || 0;
-  exec(list[index], {cwd: compilerDir }, function(error, stdout, stderr) {
-    if (error) {
-      console.log(list[index]);
-      console.error(stderr);
-      console.log(stdout);
-      return;
-    }
-
-    console.log("Command", index, "OK!");
-
-    index++;
-    if (index == list.length) {
-      getExe();
-    } else {
-      complie(list, index);
-    }
-  });
-}
-
-complie(cmd);
