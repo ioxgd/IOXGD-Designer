372f26b10c2487188d32d298b9cc82fb6d158066
Max <max30012540@hotmail.com>
2019-10-01 22:00:22 +0700

    เพิ่ม UI

---
+++ js/zoom.js
@@ -0,0 +1,44 @@
+
+let zoom = 1;
+$(function() {
+    let updateZoom = () => {
+        if (zoom < 0.1) zoom = 0.1;
+        if (zoom > 4) zoom = 4;
+        $("#workspace").css("zoom", zoom);
+        $("#zoom-size").val(`${Math.round(zoom * 100)}%`);
+    }
+
+    $("#zoom-in-btn").click(() => {
+        zoom += 0.1;
+        updateZoom();
+    });
+
+    $("#zoom-out-btn").click(() => {
+        zoom -= 0.1;
+        updateZoom();
+    });
+
+    $("#zoom-size").focus(() => {
+        $("#zoom-size").val(Math.round(zoom * 100));
+        $("#zoom-size").select();
+    });
+
+    $("#zoom-size").blur(() => {
+        zoom = parseInt($("#zoom-size").val()) / 100;
+        updateZoom();
+    });
+
+    $("#zoom-size").keypress(function(e){
+        if(e.which == 13) {
+            $(this).blur();    
+        }
+    });
+
+    $(window).on('wheel', function(event){
+        if(event.originalEvent.deltaY < 0){ // wheeled up
+            $("#zoom-in-btn").click();
+        } else { // wheeled down
+            $("#zoom-out-btn").click();
+        }
+    });
+});
